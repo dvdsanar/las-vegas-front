83e5ddf3ddf5dfdafff4ef7f94d2f911219132a7
David S <dvdsanar@gmail.com>
2022-06-09 19:12:33 +0200

    feat: add Header and Footer to app to see it on the main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
