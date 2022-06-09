dbd4caea92f8c96b0072061fc669db34447ea2ac
David S <dvdsanar@gmail.com>
2022-06-09 19:11:52 +0200

    feat: footer and style footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="enlacesFooter">
+        <a href="https://github.com/dvdsanar">David</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
