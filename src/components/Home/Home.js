dd44cd5b501dc13e309ce67b61c14405d8fd4d04
David S <dvdsanar@gmail.com>
2022-06-10 10:56:47 +0200

    feat: home and styles shet

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-contenedor">
+      <div>
+        <h1>RULETA LAS VEGAS</h1>
+      </div>
+      <div>
+        <img src="https://i0.wp.com/criptotendencia.com/wp-content/uploads/2021/02/Ruleta-online-uno-de-los-juegos-mas-buscados.jpg?fit=1400%2C663&ssl=1"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
